cc65943f9f122476fb711853e053c0e5adfb3cbc
SibinSedlan <sedlan.sibin@gmail.com>
2023-01-15 20:23:45 +0100

    Removed target folders

--- day_1_2_cal_counting/target/debug/day_1_cal_counting.d
+++
@@ -1,1 +0,0 @@
-/Users/sibin/Documents/Projects/aoc2022/day_1_2_cal_counting/target/debug/day_1_cal_counting: /Users/sibin/Documents/Projects/aoc2022/day_1_2_cal_counting/src/main.rs
--- day_1_2_cal_counting/target/debug/deps/day_1_cal_counting-960d8b5492ee857a.d
+++
@@ -1,5 +0,0 @@
-/Users/sibin/Documents/Projects/aoc2022/day_1_2_cal_counting/target/debug/deps/day_1_cal_counting-960d8b5492ee857a.rmeta: src/main.rs
-
-/Users/sibin/Documents/Projects/aoc2022/day_1_2_cal_counting/target/debug/deps/day_1_cal_counting-960d8b5492ee857a.d: src/main.rs
-
-src/main.rs:
