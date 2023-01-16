8c2f17b807e189ff645cb94adc25678f61035f90
SibinSedlan <sedlan.sibin@gmail.com>
2023-01-16 19:44:02 +0100

    Fixes

--- day_1_cal_counting/src/main.rs
+++ day_1_cal_counting/src/main.rs
@@ -1,8 +1,8 @@
 use std::io;
 
 fn main() {
-    part_1();
-    // part_2();
+    // part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -30,13 +30,14 @@ fn part_1() {
 
 fn part_2() {
     println!("Hello. How many calories are each of you carrying?");
-    let mut calories = vec![];
+    let mut calories: Vec<u32> = Vec::new();
     let mut temp_val = 0;
     let mut buffer = String::new();
     let stdin = io::stdin();
     while stdin.read_line(&mut buffer).is_ok() {
         let trimmed = buffer.trim_end();
         if trimmed == "done" {
+            calories.sort_by(|a, b| b.cmp(a));
             println!(
                 "The total number of calories carried by the top three elves is {}",
                 calories[0] + calories[1] + calories[2]
@@ -44,7 +45,6 @@ fn part_2() {
             return;
         } else if trimmed == "" {
             calories.push(temp_val);
-            calories.sort_by(|a, b| b.cmp(a));
             temp_val = 0;
         } else {
             temp_val += trimmed.parse::<u32>().unwrap();
